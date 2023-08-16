cad230dad3c326251f84211941d872b80551f6e2
Aanini <142356978+Aanini@users.noreply.github.com>
2023-08-16 00:13:37 -0500

    Initial commit

---
+++ javascripts/lista_desplegable.js
@@ -0,0 +1,9 @@
+const BotonDesplegable = document.getElementById('BotonDesplegable');
+const menu = document.getElementById('menu');
+
+BotonDesplegable.addEventListener('click', function () {
+    menu.classList.toggle('menu-visible');
+});
+
+
+
---
+++ javascripts/script.js
@@ -0,0 +1,78 @@
+const iframe = document.getElementById('myIframe');
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+const button4 = document.getElementById('button4');
+const button5 = document.getElementById('button5');
+const button6 = document.getElementById('button6');
+const button7 = document.getElementById('button7');
+const button8 = document.getElementById('button8');
+const button9 = document.getElementById('button9');
+const button10 = document.getElementById('button10');
+const button11 = document.getElementById('button11');
+const button12 = document.getElementById('button12');
+const button13 = document.getElementById('button13');
+const button14 = document.getElementById('button14');
+const button15 = document.getElementById('button15');
+const button16 = document.getElementById('button16');
+const button17 = document.getElementById('button17');
+const button18 = document.getElementById('button18');
+// Agrega más referencias a botones e iframes según sea necesario
+
+button1.addEventListener('click', () => {
+    iframe.src = 'Computacion_Uno.html';
+});
+
+button2.addEventListener('click', () => {
+    iframe.src = 'Computacion_Dos.html';
+});
+button3.addEventListener('click', () => {
+    iframe.src = 'Computacion_Tres.html';
+});
+button4.addEventListener('click', () => {
+    iframe.src = 'ingles basic.html';
+});
+button5.addEventListener('click', () => {
+    iframe.src = 'Ingles Intermedio.html';
+});
+button6.addEventListener('click', () => {
+    iframe.src = 'metodos estadisticos.html';
+});
+button7.addEventListener('click', () => {
+    iframe.src = 'Ing Grafica.html';
+});
+button8.addEventListener('click', () => {
+    iframe.src = 'Dibujo grafico.html';
+});
+button9.addEventListener('click', () => {
+    iframe.src = 'Monografia.html';
+});
+button10.addEventListener('click', () => {
+    iframe.src = 'Informes.html';
+});
+button11.addEventListener('click', () => {
+    iframe.src = 'Articulo de opinion.html';
+});
+button12.addEventListener('click', () => {
+    iframe.src = 'Ensayos.html';
+});
+button13.addEventListener('click', () => {
+    iframe.src = 'PAE Enfermeria.html';
+});
+button14.addEventListener('click', () => {
+    iframe.src = 'PA1, PA2 Y PA3 de Enfermeria.html';
+});
+button15.addEventListener('click', () => {
+    iframe.src = 'Edicion.html';
+});
+button16.addEventListener('click', () => {
+    iframe.src = 'Tripticos.html';
+});
+button17.addEventListener('click', () => {
+    iframe.src = 'Tareas de ING.html';
+});
+button18.addEventListener('click', () => {
+    iframe.src = 'Investigacion o Tesis.html';
+});
+
+// Agrega más eventos para otros botones y contenido
